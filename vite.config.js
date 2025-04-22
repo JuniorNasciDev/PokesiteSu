04b65eacc98c5ad9cea2be4606f64266359e06e3
Junior <juniornascidev@gmail.com>
2025-04-22 18:22:15 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/PokesiteSu/',
+  base: '/PokesiteSu/dist/',
   plugins: [react()]
   
 })
